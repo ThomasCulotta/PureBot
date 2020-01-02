c17b714b6f18861712a252765940f44d09506ff4
DoomZero <doomzero755@gmail.com>
2020-01-02 18:08:57 -0500

    Follow-up to the Whisper update
     Changes to be committed:
    	modified:   ScoreCommands.py

--- ScoreCommands.py
+++ ScoreCommands.py
@@ -1,20 +1,20 @@
 import random
 import pymongo
-
+ 
 from TwitchWebsocket import TwitchWebsocket
-
+ 
 from FlushPrint import ptf
-
+ 
 class ScoreCommands:
     def __init__(self, chan, ws, mongoClient):
         self.ws = ws
         self.chan = chan
-
+ 
         LBcolName = self.chan[1:] + "LB"
         self.leaderboard_col = mongoClient.QuoteBotDB[LBcolName]
         self.leaderboard_col.create_index([("user", pymongo.ASCENDING)])
         ptf(LBcolName)
-
+ 
     def Execute(self,msg):
         ptf("executing")
         if msg.message.startswith("!score"):
@@ -22,7 +22,7 @@ class ScoreCommands:
             ptf("tempscore: " + str(tempscore))
             result = None;
             result = self.leaderboard_col.find_one({"user": msg.user})
-
+ 
             if result == None:
                 score = tempscore
                 userObj = {
@@ -33,9 +33,9 @@ class ScoreCommands:
             else:
                 ptf(result)
                 score = result['score']
-
+ 
             message = f"[{msg.user}] Your pure count is : {str(score)}/100"
-
+ 
             if score == 69:
                 #message += " nice :sunglasses:"
                 message += " 😎"
@@ -45,52 +45,52 @@ class ScoreCommands:
             elif score >= 75:
                 #message += " :smiling_imp:"
                 message += " 😈"
-
-            self.ws.send_message(message)
-            return
-
+ 
+            #self.ws.send_message(message)
+            return message
+ 
         ##############################################
-
+ 
         if msg.message.startswith("!pureboard"):
             result = self.leaderboard_col.find().sort("score", 1)
-
+ 
             message = result[0]['user'] + ": " + str(result[0]['score'])
             result = result[1:5]
             for x in result:
                 message += ", " + x['user'] + ": " + str(x['score'])
-
-            self.ws.send_message(message)
-            return
-
+ 
+            #self.ws.send_message(message)
+            return message
+ 
         ##############################################
-
+ 
         if msg.message.startswith("!cursedboard"):
             result = self.leaderboard_col.find().sort("score", -1)
-
+ 
             message = result[0]['user'] + ": " + str(result[0]['score'])
             result = result[1:5]
             for x in result:
                 message += ", " + x['user'] + ": " + str(x['score'])
-
-            self.ws.send_message(message)
-            return
-
+ 
+            #self.ws.send_message(message)
+            return message
+ 
         ##############################################
-
+ 
         if msg.message.startswith("!clearboard"):
             if msg.tags['mod'] == '1' or msg.user == "doomzero":
                 self.leaderboard_col.remove({})
-                self.ws.send_message(f"[{msg.user}]: Leaderboard cleared!")
-                return
-            self.ws.send_message(f"[{msg.user}]: That command is mods-only!")
-            return
-
+                #self.ws.send_message(f"[{msg.user}]: Leaderboard cleared!")
+                return f"[{msg.user}]: Leaderboard cleared!"
+            #self.ws.send_message(f"[{msg.user}]: That command is mods-only!")
+            return f"[{msg.user}]: That command is mods-only!"
+ 
         ##############################################
-
+ 
         if msg.message.startswith("!clearscore"):
             if msg.tags['custom-reward-id'] == "769e238b-fe80-49ba-ab89-1e7e8ad75c88":
                 self.leaderboard_col.remove({"user": msg.user})
-                self.ws.send_message(f"[{msg.user}]: Your score has been cleared!")
-                return
-            self.ws.send_message(f"[{msg.user}]: That command requires spending Sushi Rolls on the custom reward!")
-            return
+                #self.ws.send_message(f"[{msg.user}]: Your score has been cleared!")
+                return f"[{msg.user}]: Your score has been cleared!"
+            #self.ws.send_message(f"[{msg.user}]: That command requires spending Sushi Rolls on the custom reward!")
+            return f"[{msg.user}]: That command requires spending Sushi Rolls on the custom reward!"
