d780fdbdd6741cef9c818778659b145a067aa282
Thomas <thomasculotta94@gmail.com>
2020-01-09 21:58:59 -0500

    doc generation has better formatting now

--- Commands_PureSushi.md
+++ Commands_PureSushi.md
@@ -1,44 +1,83 @@
-**PureSushi Channel Commands**
+# PureSushi Channel Commands
 
+---
+## CustomCommands
+
+```
 addcommand COMMAND TEXT
+
 addcommand newcom I'm a new command
-
+```
+```
 delcommand COMMAND
+
 delcommand newcom
+```
+---
+## QuoteCommands
 
+```
 quote add TEXT
+
 quote add Hi, I'm a PureSushi quote
-
+```
+```
 quote change ID TEXT
+
 quote change 12 Hi, I'm a better PureSushi quote
-
+```
+```
 quote delete ID
+
 quote delete 123
+
 quote delete last
-
+```
+```
 quote (ID)
+
 quote
+
 quote 123
+```
+---
+## ScoreCommands
 
+```
 purecount
-
+```
+```
 pureboard
-
+```
+```
 cursedboard
-
+```
+```
 clearboard
-
+```
+```
 clearscore
+```
+---
+## WhoCommands
 
+```
 who add USER TEXT
+
 who add Babotz Hello I'm a Babotz quote
-
+```
+```
 who delete USER ID
-who delete Babotz 12
 
+who delete Babotz 12
+```
+```
 who USER (ID)
+
 who Babotz
+
 who Babotz 14
+```
 
 lewdcount
 discord
--- DocGen.py
+++ DocGen.py
@@ -4,23 +4,33 @@ import json
 
 botDir = os.path.dirname(os.path.realpath(__file__))
 docFile = open(os.path.join(botDir, "Commands_PureSushi.md"), "w")
-docFile.write("**PureSushi Channel Commands**\n")
+docFile.write("# PureSushi Channel Commands\n\n")
 customCommandsFile = None
 
 for root, dirs, files in os.walk(botDir):
     for file in files:
         if file.endswith(".py"):
+            title = file.rstrip(".py")
+
             with open(os.path.join(root, file), "r") as file:
                 line = file.readline()
 
                 while line:
                     if line.lstrip().startswith("# snippet start"):
-                        docFile.write("\n")
-                        line = file.readline().lstrip()
+                        if title != None:
+                            docFile.write(f"---\n## {title}\n\n")
+                            title = None
 
+                        line = file.readline().lstrip()
+                        docFile.write("```\n")
+                        newLine = ""
                         while len(line) > 0 and line[0] == "#":
-                            docFile.write(line[2:])
+                            docFile.write(newLine + line[2:])
                             line = file.readline().lstrip()
+                            newLine = "\n"
+
+                        docFile.write("```\n")
+
                     line = file.readline()
 
         elif file == "CustomCommands.json":
