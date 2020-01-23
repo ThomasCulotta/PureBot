5153ede39b5eff3b2beda2ea2b4ffbd909dd4c6b
DoomZero <doomzero755@gmail.com>
2020-01-23 00:21:24 -0500

    Added "Clear Other" and "Steal Score"

--- ScoreCommands.py
+++ ScoreCommands.py
@@ -43,13 +43,14 @@ class ScoreCommands:
             else:
                 ptf(result)
                 score = result['score']
-                message = f"[{msg.user}] Your pure count is: {str(score)}/100"
+            
+            message = f"[{msg.user}] Your pure count is: {str(score)}/100"
 
             if score == 69:
                 message += " 😎"
-            elif score <= 25:
-                message += " 😇"
             elif score >= 75:
+                message += " 😇"
+            elif score <= 25:
                 message += " 😈"
 
             return message
@@ -98,8 +99,77 @@ class ScoreCommands:
         # snippet start
         # clearscore
         if message.startswith("clearscore"):
+
+            regmatch = re.match(r"^!clearscore (\S+?) ?$", msg.message)
+            #for the Reset Another's Score reward command
+            if msg.tags['custom-reward-id'] == "490b67dd-a8d3-494f-b605-3626358acd5c":
+                #if reward and no syntax
+                if regmatch == None:
+                    return f"[{msg.user}]: The syntax for that command is !clearscore NAME"
+                #if reward and syntax
+                if msg.user != "doomzero":
+                    return f"[{msg.user}]: That command is in testing, sorry. Only DoomZero can use it right now."
+                
+                targUser = regmatch[1].lower()
+
+                self.leaderboard_col.remove({"user": targUser})
+                return f"[{msg.user}]: You have cleared {targUser}'s score! 😈"
+            if regmatch: 
+                #if no reward and syntax
+                return f"[{msg.user}]: That command requires spending Sushi Rolls on the \"doomtest2\" custom reward!"
+            
+            #for the Score Reset reward command            
             if msg.tags['custom-reward-id'] == "769e238b-fe80-49ba-ab89-1e7e8ad75c88":
                 self.leaderboard_col.remove({"user": msg.user})
                 return f"[{msg.user}]: Your score has been cleared!"
 
-            return f"[{msg.user}]: That command requires spending Sushi Rolls on the custom reward!"
+            return f"[{msg.user}]: That command requires spending Sushi Rolls on the \"Score Reset\" custom reward!"
+
+        ##############################################
+        
+        # snippet start
+        # stealscore
+        if msg.message.startswith("!stealscore"):
+            regmatch = re.match(r"^!stealscore (\S+?) ?$", msg.message)
+            if regmatch == None:
+                ptf(f"message: [{msg.message}]")
+                return f"[{msg.user}]: The syntax for that command is !stealscore NAME"
+
+            if msg.tags['custom-reward-id'] != "14986982-3669-4e26-a3c4-bf34025e005d":
+                return f"[{msg.user}]: That command requires spending Sushi Rolls on the \"doomtest1\" custom reward!"
+
+            if msg.user != "doomzero":
+                return f"[{msg.user}]: That command is in testing, sorry. Only DoomZero can use it right now."
+
+            targUser = regmatch[1].lower()
+            msgUser = msg.user.lower()
+
+            targResult = self.leaderboard_col.find_one({"user": targUser})
+            if targResult == None:
+                return f"[{msg.user}]: That user does not have a score!"
+
+            ptf(f"targResult: {targResult}")
+            targScore = targResult['score']
+
+            if targScore == None:
+                return
+
+            userResult = self.leaderboard_col.find_one({"user": msgUser})
+            ptf(f"UserResult: {userResult}")
+            userScore = userResult['score']
+
+            if userScore == None:
+                self.leaderboard_col.remove({"user": targUser})
+                self.leaderboard_col.remove({"user": msgUser})
+                self.leaderboard_col.insert_one({"user": msgUser, "score": targScore, "createdAt": datetime.datetime.utcnow()})
+
+                return f"[{msg.user}]: You have stolen {targUser}'s score, and theirs has been reset! Your pure count is: {str(targScore)}/100"
+            
+            else:
+                self.leaderboard_col.remove({"user": targUser})
+                self.leaderboard_col.remove({"user": msgUser})
+
+                self.leaderboard_col.insert_one({"user": msgUser, "score": targScore, "createdAt": datetime.datetime.utcnow()})
+                self.leaderboard_col.insert_one({"user": targUser, "score": userScore, "createdAt": datetime.datetime.utcnow()})
+
+                return f"[{msg.user}]: You have stolen {targUser}'s score, and given them yours! Your pure count is: {str(targScore)}/100"
